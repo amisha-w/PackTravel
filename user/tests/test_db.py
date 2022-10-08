4e18e0bb2b10bef4831e84c96ebea6a940e10241
Swarnamalya M <swarnamalya65@gmail.com>
2022-10-08 14:12:21 -0400

    Added few db tests

--- user/tests/test_db.py
+++ user/tests/test_db.py
@@ -1,9 +1,9 @@
 from utils import get_client
 from django.http import HttpResponse
 from user.views import userDB
-from django.test import TransactionTestCase
+from django.test import SimpleTestCase
 
-class Testdb(TransactionTestCase):
+class Testdb(SimpleTestCase):
 
 
     def test_insert_db(self):
@@ -15,6 +15,17 @@ class Testdb(TransactionTestCase):
                  "email" : "jdwyer@ncsu.edu",
                  "password" : "jd45678",
                  "phone" : "987657890"}
+        id = userDB.insert_one(data).inserted_id
+        print("id is",id)
+        return id
+        # self.assertEqual(userDB.insert_one(data),True)
 
+    def test_find_db(self):
+        id = self.test_insert_db()
+        result= userDB.find_one({'_id':id})
+        print("res is",result)
+        self.assertEqual(result['_id'], id)
 
-        self.assertEqual(userDB.insert_one(data),True)
+    def test_delete_db(self):
+        id = self.test_insert_db()
+        userDB.delete_one({'_id':id})
