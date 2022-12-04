bbf86fbe46630acf07acb5e1ed3a7e9e974138d9
Ali Qureshi <61347679+qureshi-ali@users.noreply.github.com>
2022-12-04 14:05:51 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
+master_doc = 'index'
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
